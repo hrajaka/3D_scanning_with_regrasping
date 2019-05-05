1e90e9ce7db33576afb72013b42a0da0a693470d
Hasith <hrajaka@gmail.com>
2019-05-04 19:24:31 -0700

    fixed visualize obj

--- scanning/scripts/visualize_obj.py
+++ scanning/scripts/visualize_obj.py
@@ -1,11 +1,14 @@
+#!/home/cc/ee106b/sp19/class/ee106b-abj/python-virtual-environments/env/bin/python
+
 import trimesh
 import sys
 
 
 if __name__ == '__main__':
-    if len(sys.argv != 2):
+    if len(sys.argv) != 2:
         print("Please provide a filename")
         sys.exit()
 
+    print('Visualizing {}'.format(sys.argv[1]))
     pawn = trimesh.load(sys.argv[1])
     pawn.show()
